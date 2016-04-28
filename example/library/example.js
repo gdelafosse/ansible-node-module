c98bd1f6084baa6afa6aa83b45ce0fa2f7e77286
Guillaume Delafosse <g.delafosse@gmail.com>
2016-04-28 23:30:07 +0200

    extract arguments and return JSON object

---
+++ example/library/example.js
@@ -0,0 +1,9 @@
+#! node
+
+'use strict';
+
+const ansible = require('ansible-node-module');
+
+ansible.main((args) => {
+    return args;
+});
---
+++ index.js
@@ -0,0 +1,45 @@
+#! node
+
+'use strict';
+
+const fs = require('fs');
+const util = require('util');
+
+const argsReg = /(\w+)="(\w+)"/g;
+
+function readArguments() {
+    return new Promise((resolve, reject) => {
+        fs.readFile(process.argv[2], (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            
+            const content = data.toString('utf-8');
+            let args = {};
+            let matching;
+            while((matching = argsReg.exec(content)) != null) {
+                args[matching[1]] = matching[2];
+            }
+            resolve(args);
+        });
+    });
+}
+
+function main(callback) {
+    readArguments()
+        .then(callback)
+        .then((result) => {
+            if (util.isNullOrUndefined(result)) {
+                console.log('{}');
+            } else {
+                console.log(JSON.stringify(result));
+            }
+        })
+        .catch((err) => {
+            console.log(`{"failed":true, "msg":"${err.message}"}`);
+        });
+}
+
+module.exports = {
+    main
+};
